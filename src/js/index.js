6f0462a1f5f01fbd79eeab03e38ec898ba41869b
JulioV10 <117667674+JulioV10@users.noreply.github.com>
2023-06-15 23:19:31 +0000

    Simplecounter

--- src/js/index.js
+++ src/js/index.js
@@ -7,6 +7,23 @@ import "../styles/index.css";
 
 //import your own components
 import Home from "./component/home.jsx";
+let unidad = 0
+let decimal = 0
+let centenas = 0
+
+
+    setInterval(() => {
+        unidad++ 
+        if (unidad > 9){
+            unidad = 0
+            decimal++
+        } 
+        if (decimal > 9){
+            decimal = 0
+            centenas++
+        }
+        ReactDOM.render(<Home unidad ={unidad} decimal = {decimal} centenas = {centenas} />, document.querySelector("#app"));
+    }, 1000);
+
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
