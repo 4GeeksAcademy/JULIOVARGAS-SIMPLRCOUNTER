17d7c17500411df229ed5cb67399df3ad3dca73d
JulioV10 <117667674+JulioV10@users.noreply.github.com>
2023-06-21 22:35:39 +0000

    counter

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,19 +1,23 @@
 import React from "react";
-
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 
 //create your first component
 const Home = (props) => {
-	return (
-		<div className="text-center">
-			<h1>
-				{props.centenas}
-				{props.decimal}
-				{props.unidad}
-			</h1>
-		</div>
-	);
+  return (
+    <div className="text-center">
+      <div className="card" style={{ color: "white", padding:"100px"}}>
+        <div className="card-body" style={{ backgroundColor: "black", fontSize: "70px"}}>
+		<FontAwesomeIcon icon="fa-duotone fa-clock" />
+          {props.centenas}
+          {props.decimal}
+          {props.unidad}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
